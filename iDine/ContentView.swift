9750114ef0cde28d36efaafa8243ec36d222d945
Doug Mason <dmason@unitedfiregroup.com>
2020-03-12 13:04:03 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by dmason on 3/12/20.
+//  Copyright © 2020 United Fire Group. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
