c10242a8ad6dac607ad8e6e25fc07ccdb097a433
Doug Mason <dmason@unitedfiregroup.com>
2020-03-13 15:52:16 -0500

    Added Order and setup tabs.

--- iDine/ItemDetail.swift
+++ iDine/ItemDetail.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ItemDetail: View
 {
+    @EnvironmentObject var order: Order
     var item: MenuItem
     
     var body: some View {
@@ -25,6 +26,9 @@ struct ItemDetail: View
             }
             Text(item.description)
                 .padding()
+            Button("Order This") {
+                self.order.add(item: self.item)
+            }.font(.headline)
             Spacer()
         }
         .navigationBarTitle(Text(item.name), displayMode: .inline)
@@ -32,9 +36,11 @@ struct ItemDetail: View
 }
 
 struct ItemDetail_Previews: PreviewProvider {
+    static let order = Order()
+    
     static var previews: some View {
         NavigationView {
-            ItemDetail(item: MenuItem.example)
+            ItemDetail(item: MenuItem.example).environmentObject(order)
         }
     }
 }
---
+++ iDine/OrderView.swift
@@ -0,0 +1,45 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by dmason on 3/13/20.
+//  Copyright © 2020 United Fire Group. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View
+{
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink(destination: Text("Check out")) {
+                        Text("Place Order")
+                    }
+                }
+            }
+            .navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
