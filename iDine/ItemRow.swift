7d3c9d7b74d76c6e3afd3c8cc4b278a92cd07047
Doug Mason <dmason@unitedfiregroup.com>
2020-03-13 08:12:02 -0500

    Cleaned up ItemRow.

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -10,6 +10,14 @@ import SwiftUI
 
 struct ItemRow: View
 {
+    static let colors: [String: Color] = [
+        "D": .purple,
+        "G": .black,
+        "N": .red,
+        "S": .blue,
+        "V": .green
+    ]
+    
     var item: MenuItem
     
     var body: some View {
@@ -20,6 +28,16 @@ struct ItemRow: View
             VStack(alignment: .leading) {
                 Text(item.name).font(.headline)
                 Text("$\(item.price)")
+            }
+            Spacer()
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(Self.colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
